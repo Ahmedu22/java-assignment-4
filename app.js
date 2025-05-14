1f203ab068f6c7c9ed3fef472824ddb9e4051fe4
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-14 12:18:48 +0300

    java-assignment-4

---
+++ app.js
@@ -0,0 +1,25 @@
+// numbers
+
+let fisrtFavNum = 25;
+let secondFavNum = 52;
+console.log(fisrtFavNum + secondFavNum);
+console.log(fisrtFavNum - secondFavNum);
+console.log(fisrtFavNum * secondFavNum);
+console.log(fisrtFavNum / secondFavNum);
+console.log(fisrtFavNum % secondFavNum);
+
+// comparison operators
+
+let fisrtFavNumber = 77;
+let secondFavNumber = 88;
+console.log(fisrtFavNumber > secondFavNumber);
+console.log(fisrtFavNumber < secondFavNumber);
+console.log(fisrtFavNumber >= secondFavNumber);
+console.log(fisrtFavNumber <= secondFavNumber);
+
+console.log(77 === 88);
+console.log(77 !== 88);
+
+
+
+
